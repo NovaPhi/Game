de8785d29a4c81dedd25f58660a95fb9fd45b6d9
Alonso111204 <A01782379@tec.mx>
2026-04-25 20:36:58 -0600

    Resuelve conflictos del stash

--- Z_Attack/Api/App.js
+++ Z_Attack/Api/App.js
@@ -11,8 +11,8 @@ app.use(cors())
 const connection = mysql.createConnection({
         host: '127.0.0.1',
         user: 'root',
-        password: '1234',
-        database: 'Z_ATTACK'
+        password: 'Alo111204*',
+        database: 'z_attack'
     });
     connection.connect((err) => {
         if (err) throw err;
