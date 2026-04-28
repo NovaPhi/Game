c33ae4cbf4acadd608d4d57858f42f9e78d70da5
NovaPhi <luisjaimeariass@gmail.com>
2026-04-27 18:46:42 -0600

    tutorial fixes

--- Z_Attack/JS/cards.js
+++ Z_Attack/JS/cards.js
@@ -1,12 +1,4 @@
-(function applySettings() {
-    const brightness = localStorage.getItem('brightness') ?? 100;
-    const colorblind = localStorage.getItem('colorblind') || 'none';
 
-    document.body.style.filter = `brightness(${brightness}%)`;
-
-    document.body.classList.remove('deuteranopia', 'protanopia', 'tritanopia');
-    if (colorblind !== 'none') document.body.classList.add(colorblind);
-})();
 
 "use strict";
 
@@ -126,3 +118,11 @@ async function loadPlayerDeck() {
 
     playerStats.deck = createStarterDeck();
 }
+
+document.addEventListener('DOMContentLoaded', function applySettings() {
+    const brightness = localStorage.getItem('brightness') ?? 100;
+    const colorblind = localStorage.getItem('colorblind') || 'none';
+    document.body.style.filter = `brightness(${brightness}%)`;
+    document.body.classList.remove('deuteranopia', 'protanopia', 'tritanopia');
+    if (colorblind !== 'none') document.body.classList.add(colorblind);
+});
